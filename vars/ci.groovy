06cb744319505aa169bbec316622de392c688aeb
Ganesh <ganeshvelugula28@gmail.com>
2023-10-31 14:50:20 +0530

    uplo

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -6,22 +6,22 @@ def call(){
         stages{
             stage('complile'){
                 steps{
-
+                        echo 'compilation phase'
                 }
             }
             stage('unit test'){
                 steps{
-
+                        echo 'testingphase'
                 }
             }
             stage('Quality analysis'){
                 steps{
-
+                        echo 'quality'
                 }
             }
             stage('Uploading the code to centrialized place'){
                 steps{
-
+                        echo 'CODE UPLOad'
                 }
             }
     }
