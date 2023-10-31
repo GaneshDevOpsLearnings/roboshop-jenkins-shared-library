b51ccd969d6462f928588c6ca028a3d1f36b10f6
Ganesh <ganeshvelugula28@gmail.com>
2023-10-31 12:55:04 +0530

    pp

---
+++ vars/ci.groovy
@@ -0,0 +1,29 @@
+def call(){
+    pipeline{
+    agent{
+        label 'workstation'
+    }
+    stages{
+        stage('complile'){
+            steps{
+
+            }
+        }
+        stage('unit test'){
+            steps{
+
+            }
+        }
+        stage('Quality analysis'){
+            steps{
+
+            }
+        }
+        stage('Uploading the code to centrialized place'){
+            steps{
+
+            }
+        }
+    }
+}
+}
